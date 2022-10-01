82271a3688f94f992766f188d91e843a639de3a4
kubealex <al.rossi87@gmail.com>
2022-10-01 12:31:22 +0200

    working sample

--- src/main/java/org/acme/SensorAggregator.java
+++ src/main/java/org/acme/SensorAggregator.java
@@ -3,14 +3,14 @@ package org.acme;
 import org.acme.model.SensorData;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-import io.vertx.core.json.Json;
+import io.quarkus.logging.Log;
 import io.vertx.core.json.JsonObject;
 public class SensorAggregator {
 
     @Incoming("test-in")
     public void consume(JsonObject data){
-        SensorData test = data.mapTo(SensorData.class);
-        System.out.println(test);
+        Log.info("Received sensor data from controller");
+        SensorData sensorData = data.mapTo(SensorData.class);
         System.out.println(data.toString());
     }
 }
