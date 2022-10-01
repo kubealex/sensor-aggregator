045af28bcfbc9d1c6b551e5b3c225e6cd9547723
kubealex <al.rossi87@gmail.com>
2022-10-01 17:32:52 +0200

    added configuration

--- src/main/java/org/acme/SensorAggregator.java
+++ src/main/java/org/acme/SensorAggregator.java
@@ -1,16 +1,38 @@
 package org.acme;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.acme.model.SensorData;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.WriteApiBlocking;
+import com.influxdb.client.domain.WritePrecision;
 
 import io.quarkus.logging.Log;
 import io.vertx.core.json.JsonObject;
+@ApplicationScoped
 public class SensorAggregator {
+
+    @ConfigProperty(name = "influxdb.url",defaultValue = "http://localhost:8086")
+    String influxdbURL;
+    @ConfigProperty(name = "influxdb.token")
+    String influxdbToken;
+    @ConfigProperty(name = "influxdb.bucket")
+    String influxdbBucket;
+    @ConfigProperty(name = "influxdb.organization")
+    String influxdbOrg;
 
     @Incoming("test-in")
     public void consume(JsonObject data){
+        InfluxDBClient client = InfluxDBClientFactory.create(influxdbURL, influxdbToken.toCharArray());
+
         Log.info("Received sensor data from controller");
         SensorData sensorData = data.mapTo(SensorData.class);
-        System.out.println(data.toString());
+        WriteApiBlocking writeApi = client.getWriteApiBlocking();
+        writeApi.writeMeasurement(influxdbBucket, influxdbOrg, WritePrecision.NS, sensorData);
+        Log.info("Sensor data processed and written on InfluxDB");
     }
 }
--- src/main/java/org/acme/model/SensorData.java
+++ src/main/java/org/acme/model/SensorData.java
@@ -1,19 +1,31 @@
 package org.acme.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.influxdb.annotations.Column;
+import com.influxdb.annotations.Measurement;
+
+@Measurement(name = "SensorData")
 public class SensorData {
     @JsonProperty("sensor_location")
+    @Column(tag = true)
     private String location;
+    @Column
     @JsonProperty("sensor_data")
     private Double temperature;
+    @Column(tag = true)
     @JsonProperty("sensor_id")
     private String deviceID;
-
+    @Column(timestamp = true)
+    @JsonProperty("sensor_timestamp")
+    private Instant timestamp;
+    
     public SensorData(String location, Double temperature, String deviceID) {
         this.location = location;
         this.temperature = temperature;
         this.deviceID = deviceID;
+        this.timestamp = Instant.now();
     }
 
     public String getLocation() {
