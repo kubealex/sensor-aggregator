23a29d3302c50f9abd762c5000b68895cb582fdd
kubealex <al.rossi87@gmail.com>
2023-06-05 14:00:17 +0200

    linting

--- src/main/java/org/acme/SensorAggregator.java
+++ src/main/java/org/acme/SensorAggregator.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+
 import org.acme.model.SensorData;
 import org.acme.service.InfluxDBService;
 import org.acme.service.InfluxDBServiceException;
